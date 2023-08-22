29211f1e4a2ff7f02a699ed0230dabad011462c5
67ujuj <vauclinjulien@gmail.com>
2023-08-22 17:09:03 +0200

    fetch ville ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,25 @@ datedujour.innerHTML = today.toLocaleDateString('fr-FR', {
 });
 let myville = document.getElementById('myville');
 // On lie l'API avec la clé
-let url =
-  'https://api.openweathermap.org/data/2.5/forecast?appid=e2e45fe5701de8629a065e6427da0380&q=lille&lang=fr&units=metric';
-fetch(url)
-  .then((response) => response.json())
-  .then(function (data) {
-    console.log(data);
-    console.log(data.city['name']);
-    myville.innerHTML = data.city['name'];
-  });
+
+// fonction qui permet de clic quand on appuie sur entrée
+function toucheEntree(event) {
+  if (event.keyCode === 13) {
+    document.getElementById('bouton').click();
+  }
+}
+bouton.addEventListener('click', function () {
+  if (tag.value == '') {
+    alert('Veuillez saisir une ville');
+  } else if (tag != '') {
+    let url = `https://api.openweathermap.org/data/2.5/forecast?appid=e2e45fe5701de8629a065e6427da0380&q=${tag.value}&lang=fr&units=metric`;
+    fetch(url)
+      .then((response) => response.json())
+      .then(function (data) {
+        console.log(data);
+        console.log(data.city['name']);
+        console.log(data.cod);
+        myville.innerHTML = data.city['name'];
+      });
+  }
+});
