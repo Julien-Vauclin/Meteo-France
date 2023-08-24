9791faa7d91c4dd4c45c58bdd8102503248aa6b4
67ujuj <vauclinjulien@gmail.com>
2023-08-24 12:16:34 +0200

    couleur scrollzone ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,6 @@ bouton.addEventListener('click', function () {
           wind.innerHTML = '';
           icon.innerHTML = '';
         } else {
-          console.log(data.city['name']);
-          console.log(data.cod);
           let nomville = data.city['name'];
           myville.innerHTML = nomville.toUpperCase();
           let description = data.list[0]['weather'][0]['description'];
@@ -60,28 +58,95 @@ bouton.addEventListener('click', function () {
             `<img src="https://openweathermap.org/img/wn/` +
             data.list[0]['weather'][0]['icon'] +
             `@2x.png">`;
+          //Déclaration variables heure icon et temperature
           let heure1 = document.getElementById('heure1');
-          let icon1 = document.getElementById('icon1');
-          let temp1 = document.getElementById('temp1');
           let heure2 = document.getElementById('heure2');
+          let heure3 = document.getElementById('heure3');
+          let heure4 = document.getElementById('heure4');
+          let heure5 = document.getElementById('heure5');
+          let heure6 = document.getElementById('heure6');
+          let heure7 = document.getElementById('heure7');
+          let heure8 = document.getElementById('heure8');
+          let icon1 = document.getElementById('icon1');
           let icon2 = document.getElementById('icon2');
+          let icon3 = document.getElementById('icon3');
+          let icon4 = document.getElementById('icon4');
+          let icon5 = document.getElementById('icon5');
+          let icon6 = document.getElementById('icon6');
+          let icon7 = document.getElementById('icon7');
+          let icon8 = document.getElementById('icon8');
+          let temp1 = document.getElementById('temp1');
           let temp2 = document.getElementById('temp2');
-
-          heure1.innerHTML = data.list[0]['dt_txt'].slice(11, 16);
+          let temp3 = document.getElementById('temp3');
+          let temp4 = document.getElementById('temp4');
+          let temp5 = document.getElementById('temp5');
+          let temp6 = document.getElementById('temp6');
+          let temp7 = document.getElementById('temp7');
+          let temp8 = document.getElementById('temp8');
+          // heure 1
+          heure1.innerHTML = 'Actuellement';
           icon1.innerHTML =
             `<img src="https://openweathermap.org/img/wn/` +
             data.list[0]['weather'][0]['icon'] +
             `@2x.png">
           `;
           temp1.innerHTML = Math.round(data.list[0]['main']['temp']) + '°C';
+          // heure 2
           heure2.innerHTML = data.list[1]['dt_txt'].slice(11, 16);
           icon2.innerHTML =
             `<img src="https://openweathermap.org/img/wn/` +
             data.list[1]['weather'][0]['icon'] +
             `@2x.png">
           `;
-          // On affiche la température en arrondissant à l'entier le plus proche
           temp2.innerHTML = Math.round(data.list[1]['main']['temp']) + '°C';
+          // heure 3
+          heure3.innerHTML = data.list[2]['dt_txt'].slice(11, 16);
+          icon3.innerHTML =
+            `<img src="https://openweathermap.org/img/wn/` +
+            data.list[2]['weather'][0]['icon'] +
+            `@2x.png">
+          `;
+          temp3.innerHTML = Math.round(data.list[2]['main']['temp']) + '°C';
+          // heure 4
+          heure4.innerHTML = data.list[3]['dt_txt'].slice(11, 16);
+          icon4.innerHTML =
+            `<img src="https://openweathermap.org/img/wn/` +
+            data.list[3]['weather'][0]['icon'] +
+            `@2x.png">
+          `;
+          temp4.innerHTML = Math.round(data.list[3]['main']['temp']) + '°C';
+          // heure 5
+          heure5.innerHTML = data.list[4]['dt_txt'].slice(11, 16);
+          icon5.innerHTML =
+            `<img src="https://openweathermap.org/img/wn/` +
+            data.list[4]['weather'][0]['icon'] +
+            `@2x.png">
+          `;
+          temp5.innerHTML = Math.round(data.list[4]['main']['temp']) + '°C';
+          // heure 6
+          heure6.innerHTML = data.list[5]['dt_txt'].slice(11, 16);
+          icon6.innerHTML =
+            `<img src="https://openweathermap.org/img/wn/` +
+            data.list[5]['weather'][0]['icon'] +
+            `@2x.png">
+          `;
+          temp6.innerHTML = Math.round(data.list[5]['main']['temp']) + '°C';
+          // heure 7
+          heure7.innerHTML = data.list[6]['dt_txt'].slice(11, 16);
+          icon7.innerHTML =
+            `<img src="https://openweathermap.org/img/wn/` +
+            data.list[6]['weather'][0]['icon'] +
+            `@2x.png">
+          `;
+          temp7.innerHTML = Math.round(data.list[6]['main']['temp']) + '°C';
+          // heure 8
+          heure8.innerHTML = data.list[7]['dt_txt'].slice(11, 16);
+          icon8.innerHTML =
+            `<img src="https://openweathermap.org/img/wn/` +
+            data.list[7]['weather'][0]['icon'] +
+            `@2x.png">
+          `;
+          temp8.innerHTML = Math.round(data.list[7]['main']['temp']) + '°C';
         }
       });
   }
